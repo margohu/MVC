f66341be27e95c3b2f141d8215e475054d6af6e1
margohu <marxhytt@gmail.com>
2023-03-03 16:16:55 +0200

    As a user I can add my friends name and birthday to input-boxes [Delivers #184614237]

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'My Friends Birthdays' });
 });
 
+/* GET add birthday page. */
+router.get('/addBirthday', function(req, res, next) {
+  res.render('addBirthday', { title: 'Add birthday' });
+});
+
+/* POST add birthday. */
+router.post('/addBirthday', function(req, res, next) {
+  var memoTitle = req.body.memoTitle;
+  var memoContent = req.body.memoContent;
+
+  // Create a new birthday object and add it to the birthdays array
+  var newMemo = { title: memoTitle, content: memoContent };
+  seeAll.push(newMemo);
+
+  // Redirect the user to the birthday page- does not work yet but needed in next user-story
+  res.redirect('/seeAll');
+});
+
 module.exports = router;
